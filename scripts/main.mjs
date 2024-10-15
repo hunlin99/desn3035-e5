e1b4aeb4bd27d7f2185ce2f3648523024278a00f
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-10-15 17:19:26 +0000

    Puppeteer & lighthouse

---
+++ scripts/main.mjs
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import lighthouse from 'lighthouse';
+import puppeteer from 'puppeteer';
+
+// Or import puppeteer from 'puppeteer-core';
+
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+// Navigate the page to a URL.
+await page.goto('https://hunlin99.github.io/desn3035-e5/');
+
+// Set screen size.
+await page.setViewport({width: 1080, height: 1024});
+await page.waitForNetworkIdle();
+await page.screenshot({
+    path:"auto_screenshot.png"
+})
+
+const options = {
+    output: 'html'
+};
+const runnerResult = await lighthouse('https://hunlin99.github.io/desn3035-e5/', options, undefined, page);
+const reportHtml = runnerResult.report;
+fs.writeFileSync('auto_report.html', reportHtml);
+
+await browser.close();
